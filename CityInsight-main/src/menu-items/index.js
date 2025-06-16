e510b35d375a6c46d2a887948081e9bfea4c4f0a
ArhamSaif <arhamsaif65@gmail.com>
2025-06-16 15:50:11 +0500

    .

--- CityInsight-main/src/menu-items/index.js
+++ CityInsight-main/src/menu-items/index.js
@@ -4,11 +4,12 @@ import Ads from './ads'
 import Products from './Products';
 import Permissions from './Permissions'
 import User from './User'
+import Other from './other'
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, Categories, Products, Permissions, Ads, User]
+  items: [dashboard, Categories, Products, Permissions, Ads,Other, User]
 };
 
 export default menuItems;
